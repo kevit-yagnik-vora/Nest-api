ba09f070f36bf83c7d7dce83b5305e44d8098fc1
kevit-yagnik-vora <yagnik.vora@kevit.io>
2025-09-23 12:11:18 +0530

    dashboard component modified

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,26 +1,36 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Campaign, CampaignSchema } from 'src/campaign/schemas/campaign.schema';
-import { Contact, ContactSchema } from 'src/contact/schemas/contact.schema';
+import { DashboardService } from './dashboard.service';
+import { DashboardController } from './dashboard.controller';
 import { User, UserSchema } from 'src/user/schemas/user.schema';
+import { Contact, ContactSchema } from 'src/contact/schemas/contact.schema';
+import {
+  MessageTemplate,
+  MessageTemplateSchema,
+} from 'src/message-template/schemas/message-template.schema';
+import { Campaign, CampaignSchema } from 'src/campaign/schemas/campaign.schema';
+import {
+  CampaignMessage,
+  CampaignMessageSchema,
+} from 'src/campaign/schemas/campaign-message.schema';
 import {
   Workspace,
   WorkspaceSchema,
 } from 'src/workspace/schemas/workspace.schema';
-import { DashboardController } from './dashboard.controller';
-import { DashboardService } from './dashboard.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    // Import all the schemas we need to query for the dashboard
     MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
       { name: Workspace.name, schema: WorkspaceSchema },
+      { name: User.name, schema: UserSchema },
       { name: Contact.name, schema: ContactSchema },
+      { name: MessageTemplate.name, schema: MessageTemplateSchema },
       { name: Campaign.name, schema: CampaignSchema },
+      { name: CampaignMessage.name, schema: CampaignMessageSchema },
     ]),
   ],
+  providers: [DashboardService, JwtService],
   controllers: [DashboardController],
-  providers: [DashboardService],
 })
 export class DashboardModule {}
